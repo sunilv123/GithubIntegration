fd06157acb048c6208951a415f846e956f7c5a17
sunilkumarverma <sunil@thrymr.net>
2017-07-31 15:31:50 +0530

    second commit

---
+++ src/main/java/git/net/GitHub/controller/GitHubController.java
@@ -0,0 +1,17 @@
+package git.net.GitHub.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class GitHubController {
+
+	@PostMapping("/api/github-activity-url")
+	public void testGitHubCommit(@RequestBody String jsonData){
+		System.out.println("-jsonData------"+jsonData.toString());
+	}
+	
+}
