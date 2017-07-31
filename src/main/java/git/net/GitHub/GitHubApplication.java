ea45588ba8d05ac44ce85189859c47df774f0607
sunilkumarverma <sunil@thrymr.net>
2017-07-31 15:16:41 +0530

    first commit

---
+++ src/main/java/git/net/GitHub/GitHubApplication.java
@@ -0,0 +1,12 @@
+package git.net.GitHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubApplication.class, args);
+	}
+}
